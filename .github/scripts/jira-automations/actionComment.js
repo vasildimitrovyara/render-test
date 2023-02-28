ce37f68b4d7edf41eae4db5afb982a9a785cdbd0
vasildimitrovyara <vassil.dimitrov@yara.com>
2023-02-28 12:24:04 +0200

    RT-1 some message 6

--- .github/scripts/jira-automations/actionComment.js
+++ .github/scripts/jira-automations/actionComment.js
@@ -1,70 +1,15 @@
-const executeTeamAction = require('./utils/executeTeamAction');
-const findBranchThroughPR = require('../utils/findBranchThroughPR');
+const addRenderLink = require('../commands/addRenderLink.js');
 
-const extractRenderLink = (comment) => {
-  const urlRegex = /(https?:\/\/[^\s]+)/g;
-  const urls = comment.match(urlRegex);
-
-  const hasMatchingUrls = urls && urls.length;
-  if (!hasMatchingUrls) return null;
-
-  const renderUrl = urls.find((url) => url.includes('onrender.com'));
-
-  if (!renderUrl) return null;
-
-  if (renderUrl[renderUrl.length - 1] === '.') {
-    return renderUrl.slice(0, -1);
-  }
-
-  return renderUrl;
-};
-
-const setRenderLinkComment = async ({ renderLink }) => {
-  const bodyData = {
-    body: {
-      type: 'doc',
-      version: 1,
-      content: [
-        {
-          type: 'paragraph',
-          content: [
-            {
-              text: 'Render link: ',
-              type: 'text',
-            },
-            {
-              type: 'text',
-              text: renderLink,
-              marks: [
-                {
-                  type: 'link',
-                  attrs: {
-                    href: renderLink,
-                    title: 'OnRender Link',
-                  },
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    },
-  };
-};
-
-module.exports = async ({ github, context, actionName }) => {
+module.exports = async ({ context }) => {
   setTimeout(async () => {
     try {
       const { comment } = context.payload;
       if (!comment || !comment.body) {
         throw new Error('Comment missing');
       } else {
-        const renderLink = extractRenderLink(comment.body);
-        const renderLinkComment = setRenderLinkComment(renderLink);
+        const renderLinkComment = addRenderLink('RT-1', comment.body);
       }
       
-      console.log(renderLinkComment);
-      return renderLinkComment
     } catch (e) {
       console.error(e);
     }
