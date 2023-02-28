2098158fdf4706323f85f551934f30448ca3e219
vasildimitrovyara <vassil.dimitrov@yara.com>
2023-02-28 15:50:31 +0200

    RT-1 some message 6

--- .github/scripts/formatRenderComment.js
+++ .github/scripts/formatRenderComment.js
@@ -1,18 +1,18 @@
 module.exports = ({context}) => {
-    console.log(context.payload.body)
+    console.log(context)
     // return context.payload.body;
-    const comment = context.payload.body;
-    const urlRegex = /(https?:\/\/[^\s]+)/g;
-    const urls = comment.match(urlRegex);
+    // const comment = context.payload.body;
+    // const urlRegex = /(https?:\/\/[^\s]+)/g;
+    // const urls = comment.match(urlRegex);
   
-    const hasMatchingUrls = urls && urls.length;
-    if (!hasMatchingUrls) return null;
+    // const hasMatchingUrls = urls && urls.length;
+    // if (!hasMatchingUrls) return null;
   
-    const renderUrl = urls.find((url) => url.includes('onrender.com'));
+    // const renderUrl = urls.find((url) => url.includes('onrender.com'));
   
-    if (!renderUrl) return null;
-    if (renderUrl[renderUrl.length - 1] === '.') {
-      return renderUrl.slice(0, -1);
-    } 
-    return renderUrl;
+    // if (!renderUrl) return null;
+    // if (renderUrl[renderUrl.length - 1] === '.') {
+    //   return renderUrl.slice(0, -1);
+    // } 
+    // return renderUrl;
   };
