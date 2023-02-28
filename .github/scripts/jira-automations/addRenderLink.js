7781e005e732dd1886be5eacc7440f99224e42b2
vasildimitrovyara <vassil.dimitrov@yara.com>
2023-02-28 10:00:00 +0200

    RT-1 some message 6

--- .github/scripts/jira-automations/addRenderLink.js
+++ .github/scripts/jira-automations/addRenderLink.js
@@ -1,16 +1,17 @@
 const Utils = require('../utils/utils');
 
 module.exports = ({ github, context, actionName }) => {
-    const branchName = new Utils().findBranchThroughPR({        
-        github,
-        repo: context.payload.repository.name,
-        owner: context.payload.repository.owner.login,
-        issueNumber: context.payload.number
-    });
-    const { comment } = context.payload;
-    
-    if (!comment || !comment.body) {
-      throw new Error('Comment missing');
-    }
+  const utils = new Utils();
+  const branchName = utils.findBranchThroughPR({        
+      github,
+      repo: context.payload.repository.name,
+      owner: context.payload.repository.owner.login,
+      issueNumber: context.payload.number
+  });
+  const { comment } = context.payload;
+  
+  if (!comment || !comment.body) {
+    throw new Error('Comment missing');
+  }
 }
 
