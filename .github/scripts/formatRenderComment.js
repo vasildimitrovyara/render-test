df3e98abd85d0b35304fb694fe5689aad6378bb4
vasildimitrovyara <vassil.dimitrov@yara.com>
2023-03-01 09:08:47 +0200

    RT-1 some message 6

--- .github/scripts/formatRenderComment.js
+++ .github/scripts/formatRenderComment.js
@@ -1,20 +1,17 @@
 module.exports = ({context}) => {
-    console.log(context);
     const comment = context.payload.comment.body;
     const urlRegex = /(https?:\/\/[^\s]+)/g;
     const urls = comment.match(urlRegex);
     const hasMatchingUrls = urls && urls.length;
+    const renderUrl = urls?.find((url) => url?.includes('onrender.com'));
     let jiraComment = '';
 
     if (!hasMatchingUrls) return null;
-  
-    const renderUrl = urls.find((url) => url.includes('onrender.com'));
-  
-    if (!renderUrl) return null;
-    else {
+    if (renderUrl) {
       const strippedLink = renderUrl.replace(`-pr-${context.issue.number}`,'');
-
+      
       jiraComment = `Render link: ${strippedLink}`;
     }
+    if (!renderUrl) return null;
     return jiraComment;
   };
