2623912590aa118f42efea2de28a46ae65ee5ab1
vasildimitrovyara <vassil.dimitrov@yara.com>
2023-03-02 16:56:43 +0200

    fix(RT-1): some text

--- .github/scripts/addCommentToTicket.js
+++ .github/scripts/addCommentToTicket.js
@@ -4,9 +4,39 @@ module.exports = async ({ comment }) => {
   console.log(comment);
   try {
     const path = `/rest/api/3/issue/RT-1/comment`;
+    const bodyData = {
+      body: {
+        type: 'doc',
+        version: 1,
+        content: [
+          {
+            type: 'paragraph',
+            content: [
+              {
+                text: 'Render link: ',
+                type: 'text',
+              },
+              {
+                type: 'text',
+                text: renderLink,
+                marks: [
+                  {
+                    type: 'link',
+                    attrs: {
+                      href: renderLink,
+                      title: 'OnRender Link',
+                    },
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    };
     const options = {
       path,
-      data: 'SOMEEEEEE TEXTTTTT',
+      data: bodyData,
     };
 
 
