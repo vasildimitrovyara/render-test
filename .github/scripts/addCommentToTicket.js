a886c8e8bc7f1bd3db04d7e8960698190212d525
vasildimitrovyara <vassil.dimitrov@yara.com>
2023-03-02 15:51:21 +0200

    fix(RT-1): some text

--- .github/scripts/addCommentToTicket.js
+++ .github/scripts/addCommentToTicket.js
@@ -1,6 +1,7 @@
 const jiraService = require('./jiraService');
 
 module.exports = async ({ ticketId, comment }) => {
+  console.log(ticketId, comment)
   try {
     const path = `/rest/api/3/issue/${ticketId}/comment`;
     const options = {
