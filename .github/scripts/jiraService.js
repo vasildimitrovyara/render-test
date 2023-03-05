331fb711e08076b473b21c413393e314be529fde
vasildimitrovyara <vassil.dimitrov@yara.com>
2023-03-05 15:47:32 +0200

    fix(RT-1): some text

--- .github/scripts/jiraService.js
+++ .github/scripts/jiraService.js
@@ -20,31 +20,31 @@ module.exports = async ({ data = '', path, method = 'POST' }) =>
         path 
       },
       (res) => {
-        let rawData = '';
+        // let rawData = '';
 
-        res.setEncoding('utf8');
+        // res.setEncoding('utf8');
 
-        res.on('data', (chunk) => {
-          rawData += chunk;
-        });
+        // res.on('data', (chunk) => {
+        //   rawData += chunk;
+        // });
 
-        res.on('end', () => {
-          try {
-            const parsedData = rawData ? JSON.parse(rawData) : null;
-            if (parsedData && (parsedData.errorMessages || []).length) {
-              throw new Error(parsedData.errorMessages[0]);
-            }
-            resolve(parsedData);
-          } catch (e) {
-            reject(e);
-          }
-        });
+        // res.on('end', () => {
+        //   try {
+        //     const parsedData = rawData ? JSON.parse(rawData) : null;
+        //     if (parsedData && (parsedData.errorMessages || []).length) {
+        //       throw new Error(parsedData.errorMessages[0]);
+        //     }
+        //     resolve(parsedData);
+        //   } catch (e) {
+        //     reject(e);
+        //   }
+        // });
       },
     );
-    req.on('error', (e) => {
-      console.error(e);
-      reject(e);
-    });
+    // req.on('error', (e) => {
+    //   console.error(e);
+    //   reject(e);
+    // });
     req.write(data);
     req.end();
   });
