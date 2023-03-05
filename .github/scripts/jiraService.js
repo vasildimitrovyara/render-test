7ec1219ec3dc20dfe5ba56434f870f3870738597
vasildimitrovyara <vassil.dimitrov@yara.com>
2023-03-05 15:39:58 +0200

    fix(RT-1): some text

--- .github/scripts/jiraService.js
+++ .github/scripts/jiraService.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch')
+const https = require('https');
 
 const port = 443;
 const hostname = 'yaradigitalfarming.atlassian.net';
@@ -10,11 +10,43 @@ const headers = {
   ).toString('base64')}`,
 };
 
-module.exports = async ({path, data}) => {
-    return fetch('addComment', {
-      pathname: path,
-    }, {
-      method: 'POST',
-      body: data,
-    })
-}
+module.exports = async ({ data = '', path, method = 'POST' }) =>
+  new Promise((resolve, reject) => {
+    const req = https.request({ 
+      headers,
+      port, 
+      method,
+      hostname,
+      path 
+    },
+      (res) => {
+        let rawData = '';
+
+        res.setEncoding('utf8');
+
+        res.on('data', (chunk) => {
+          rawData += chunk;
+        });
+
+        res.on('end', () => {
+          try {
+            const parsedData = rawData ? JSON.parse(rawData) : null;
+            if (parsedData && (parsedData.errorMessages || []).length) {
+              throw new Error(parsedData.errorMessages[0]);
+            }
+            resolve(parsedData);
+          } catch (e) {
+            reject(e);
+          }
+        });
+      },
+    );
+
+    req.on('error', (e) => {
+      console.error(e);
+      reject(e);
+    });
+
+    req.write(data);
+    req.end();
+  });
